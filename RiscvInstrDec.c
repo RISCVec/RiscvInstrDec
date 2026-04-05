c93bdfad7214e9b5b684df30e1fe806a2ba01b2d
RISCVec <165450259+RISCVec@users.noreply.github.com>
2026-04-05 09:40:30 +0800

    Update RiscvInstrDec.c
    use stdint lib

--- RiscvInstrDec.c
+++ RiscvInstrDec.c
@@ -1,25 +1,34 @@
-#include "RiscvInstrDec.h"
+#include "RVInstDec.h"
 
-int main()
+int main( void )
 {
-   printf("\n********** RISC-V Instruction Decode **********\n");
-   unsigned int instr;
-   printf(" Please input Instruction: ");
-   scanf("%x", &instr);
+   printf( "\n********** RISC-V RV32I Instruction Decode **********" );
+   uint32_t instr;
+   while( 1 ) {
+      printf( "\n Please input Instruction ( input 0 to quit ): " );
+      scanf( "%x", &instr );
 
-   printf("\n");
-   printf(" Your Instruction is : 0x%08x\n", instr);
-   printf(" Binary Type is      : ");
-   print_binary(instr);
+      if( instr == 0 )
+      {
+         printf( "\n Quit.\n" );
+         break;
+      }
+         
+      printf( "\n" );
+      printf( " Your Instruction is : 0x%08x\n", instr );
+      printf( " Binary Type is      : " );
+      print_binary( instr );
 
-   // instruction struct declaration and init
-   struct Instruction struInstr;
-   memset(&struInstr, 0, sizeof(struct Instruction));
+      // instruction struct declaration and init
+      struct Instruction struInstr;
+      memset( &struInstr, 0, sizeof( struct Instruction ) );
 
-   // parse every seg to instruction struct
-   parse_instr_to_struct(instr, &struInstr);
-   // parse instruction struct
-   parse_instr_struct(struInstr);
+      // parse every seg to instruction struct
+      parse_instr_to_struct( instr, &struInstr );
+      // parse instruction struct
+      parse_instr_struct( struInstr );
+   }
+   
 
    return 0;
 }
